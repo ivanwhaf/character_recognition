22794ef0b5548ed3037ea7cf06c2f1b113862da1
Ivan <874869814@qq.com>
2018-02-01 11:13:27 +0800

    图像文字识别

--- character_recognition.py
+++ character_recognition.py
@@ -2,8 +2,10 @@ import pytesseract
 from PIL import Image
 import os
 
+file_path='0001.jpg'
+
 print('开始转换……')
-text=pytesseract.image_to_string(Image.open('0001.jpg'),lang='chi_sim')
+text=pytesseract.image_to_string(Image.open(file_path),lang='chi_sim')
 print('文件转换完毕')
 text = ''.join(text.split())
 f=open('text.txt','w')
